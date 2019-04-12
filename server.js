69692e8ec33eef127eeb0279960eef105383a2c2
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-04-12 17:02:58 +0100

    sever update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ mongoose
   .catch(err => console.log(err));
 
 //serve static assets
-if (process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
