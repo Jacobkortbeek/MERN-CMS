72208ebc7cfa045f2d0d581b1918e6fc7ad54652
jacobkortbeek <kortbeekdesigns@gmail.com>
2019-04-12 16:28:27 +0100

    new commit

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
+const path = require('path');
+
+const app = express();
+
+//body parser middleware
+app.use(express.json());
+
+//Mogno config
+const db = config.get('mongURI');
+
+//Connect to Mongo using Mongoose
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log(err));
+
+//serve static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
